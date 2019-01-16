9884ffad279bf05011b04dce82172db996819ac5
Nicena Fontaine <nicenas.email@gmail.com>
2019-01-16 15:06:34 -0500

    Turn politician object into a factory object and create 2 objects from this.

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
-// Politician Object
+// Create Politician Factory Object
 
-var politician = {};
-politician.name = "";
-politician.electionResults = null;
-politician.totalVotes = 0;
+var createPolitician = function (polName) {
+    var politician = {};
+    politician.name = polName;
+    politician.electionResults = null;
+    politician.totalVotes = 0;
+
+    return politician;
+};
+
+var candidate1 = createPolitician('George');
+var candidate2 = createPolitician('Julie');
